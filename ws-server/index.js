e40e0ec2678b23c3d9efeae3b40df4960a80ad13
PWhiddy <all.cows.like.to.moo@gmail.com>
2024-03-25 14:20:25 -0400

    ws server update - 'anotha one'

--- ws-server/index.js
+++ ws-server/index.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const expressWs = require('express-ws');
 
+
+console.log("node version:");
+console.log(process.version);
+
+
 const app = express();
 expressWs(app);
 
 const broadcasters = new Set();
 const receivers = new Set();
 
+function doGC() {
+        if (global.gc) {
+            global.gc();
+        } else {
+            console.log('Garbage collection unavailable.  Pass --expose-gc '
+              + 'when launching node to enable forced garbage collection.');
+        }
+        setTimeout(doGC, 60000);
+}
+
+setTimeout(doGC, 60000);
+
+function updateStats() {
+  receivers.forEach(receiver => {
+      //if (receiver.readyState === WebSocket.OPEN) {
+      receiver.send(JSON.stringify({"stats": {envs: broadcasters.size, viewers: receivers.size}}));
+      //}
+  });
+}
+
+setInterval(updateStats, 10000);
+
 app.ws('/broadcast', function(ws, req) {
   broadcasters.add(ws);
-  console.log('A new broadcaster connected.');
+  //console.log('A new broadcaster connected.');
 
   ws.on('message', function(message) {
-    console.log('Broadcasting: %s', message);
+    //console.log('Broadcasting: %s', message);
     receivers.forEach(receiver => {
       //if (receiver.readyState === WebSocket.OPEN) {
       receiver.send(message);
@@ -22,20 +49,19 @@ app.ws('/broadcast', function(ws, req) {
 
   ws.on('close', () => {
     broadcasters.delete(ws);
-    console.log('Broadcaster disconnected');
+    //console.log('Broadcaster disconnected');
   });
 });
 
 app.ws('/receive', function(ws, req) {
   receivers.add(ws);
-  console.log('A new receiver connected.');
+  //console.log('A new receiver connected.');
 
   ws.on('close', () => {
     receivers.delete(ws);
-    console.log('Receiver disconnected');
+    //console.log('Receiver disconnected');
   });
 });
 
-const port = 8080;
+const port = 3344;
 app.listen(port, () => console.log(`WebSocket server started on ws://localhost:${port}`));
-
