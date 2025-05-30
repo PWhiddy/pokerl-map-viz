c854fda07c1a56a0029b2b2b74d6484f9e1a239c
PWhiddy <all.cows.like.to.moo@gmail.com>
2025-05-30 16:22:25 -0400

    don't ask before overwriting videos

--- large-scale-viz/generate_all_videos.sh
+++ large-scale-viz/generate_all_videos.sh
@@ -6,7 +6,7 @@ versions=("extra_fast" "fast" "medium" "full")
 # Loop through each version and run the ffmpeg command
 for version in "${versions[@]}"; do
     echo "Processing version: $version"
-    ffmpeg -framerate 60 -i images/coord_map_${version}_%d.exr \
+    ffmpeg -y -framerate 60 -i images/coord_map_${version}_%d.exr \
     -vf "crop=512:512:0:0,curves=all='0/0 0.0002/0.01 1/1',scale=iw*8:ih*8:flags=neighbor" \
     -c:v prores_ks -profile:v 3 "output_${version}.mov"
 done
